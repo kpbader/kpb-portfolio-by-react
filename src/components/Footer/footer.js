9c99c74787611a4ac07fd7b120d8632038298833
Kyle Patrick Bader <kbader91@gmail.com>
2022-05-14 17:21:52 -0700

    added in github and deployed app links with images

--- src/components/Footer/footer.js
+++ src/components/Footer/footer.js
@@ -2,8 +2,10 @@
 function footer(props) {
     return (
         <footer>
+            <div className="profiles-label">Profiles</div>
             <a href="https://github.com/kpbader" target="_blank" rel="noopener noreferrer">Github</a>
             <a href="https://www.linkedin.com/in/kyle-patrick-bader-9165601aa/" target="_blank" rel="noopener noreferrer">LinkedIn</a>
+            <a href="https://stackoverflow.com/users/17444256/kpbader" target="_blank" rel="noopener noreferrer">Stack Overflow</a>
         </footer>
     );
 }
--- src/components/Projects/projects.js
+++ src/components/Projects/projects.js
@@ -1,18 +1,44 @@
 import kit from '../../assets/images/kit_screenshot_final.png'
-import notmonday from '../../assets/images/notmondaydotcom.png'
+import notetaker from '../../assets/images/note-taker-screenshot.png'
 import weather from '../../assets/images/tell_me_the_weather.png'
 import quiz from '../../assets/images/rapid-quiz-screenshot.png'
 import workday from '../../assets/images/workDaySched_screenshot.png'
+import passwordgenerator from '../../assets/images/password-generator-screenshot.png'
 
 function portfolio(props) {
     return (
-        <div id="projects">
-            <img src={kit} className="kit"/>
-            <img src={notmonday} className="notmonday" />
-            <img src={weather} className="weather"/>
-            <img src={quiz} className="quiz"/>
-            <img src={workday} className="workday"/>
-        </div>
+        <section id="projects">
+            <div id="kit">
+                <a href="https://blooming-anchorage-21473.herokuapp.com/" target="_blank" rel="noopener noreferrer">Deployed Application</a>
+                <a href="https://github.com/kpbader/kit" target="_blank" rel="noopener noreferrer">Github repository</a>
+                <img src={kit} className="kit-image" alt="Screenshot of group social media project" />
+            </div>
+            <div id="note-taker">
+                <a href="https://kpbnotetaker-app.herokuapp.com/" target="_blank" rel="noopener noreferrer">Deployed Application</a>
+                <a href="https://github.com/kpbader/note-taker-express" target="_blank" rel="noopener noreferrer">Github repository</a>
+                <img src={notetaker} className="notmonday" alt="Screenshot of an employee hub project" />
+            </div>
+            <div id="weather-app">
+                <a href="https://github.com/kpbader/tell-me-the-weather" target="_blank" rel="noopener noreferrer">Deployed Application</a>
+                <a href="https://github.com/kpbader/tell-me-the-weather" target="_blank" rel="noopener noreferrer">Github repository</a>
+                <img src={weather} className="weather" alt="Screenshot of a weather app using open weather api" />
+            </div>
+            <div id="rapid-quiz">
+                <a href="https://kpbader.github.io/rapid-quiz/" target="_blank" rel="noopener noreferrer">Deployed Application</a>
+                <a href="https://github.com/kpbader/rapid-quiz" target="_blank" rel="noopener noreferrer">Github repository</a>
+                <img src={quiz} className="quiz" alt="Screenshot of a timed quiz app" />
+            </div>
+            <div id="workday">
+                <a href="https://kpbader.github.io/digital-daily-planner/" target="_blank" rel="noopener noreferrer">Deployed Application</a>
+                <a href="https://github.com/kpbader/digital-daily-planner" target="_blank" rel="noopener noreferrer">Github repository</a>
+                <img src={workday} className="workday" alt="Screenshot of a daily planner app" />
+            </div>
+            <div id="password-generator">
+                <a href="https://kpbader.github.io/shuffle-the-deck-/" target="_blank" rel="noopener noreferrer">Deployed Application</a>
+                <a href="https://github.com/kpbader/shuffle-the-deck-" target="_blank" rel="noopener noreferrer">Github repository</a>
+                <img src={passwordgenerator} className="workday" alt="Screenshot of a running and coaching organization webpage" />
+            </div>
+        </section>
     );
 }
 
