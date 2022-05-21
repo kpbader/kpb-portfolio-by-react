7c5f3604af7d029d6d92fafabba56c7bff4194ea
Kyle Patrick Bader <kbader91@gmail.com>
2022-05-21 10:47:48 -0700

    highlighted tabs effect implemented

--- src/components/Nav/nav.js
+++ src/components/Nav/nav.js
@@ -6,14 +6,24 @@ function Nav() {
         const location = useLocation() 
         console.log(location.pathname);
 
+        const selectedTab = location.pathname;
+
+        function activeTab(path) {
+            if (selectedTab !== path) {
+                return;
+            } else {
+                return {color: "silver"}
+            }
+        }
+
     return (
         <section id="nav-titles">
             <ul id="title-list">
-            <Link to="/kpb-portfolio-by-react" className="title">Home</Link>
-            <Link to="/About" className="title">About</Link>
-            <Link to="/Projects" className="title">Projects</Link>
-            <Link to="/Resume" className="title">Resume</Link>
-            <Link to="/Contact" className="title">Contact</Link>
+            <Link to="/kpb-portfolio-by-react" style={activeTab("/kpb-portfolio-by-react")}  className="title">Home</Link>
+            <Link to="/About" style={activeTab("/About")} className="title">About</Link>
+            <Link to="/Projects" style={activeTab("/Projects")} className="title">Projects</Link>
+            <Link to="/Resume" style={activeTab("/Resume")} className="title">Resume</Link>
+            <Link to="/Contact" style={activeTab("/Contact")} className="title">Contact</Link>
             </ul>
         </section>
     )
