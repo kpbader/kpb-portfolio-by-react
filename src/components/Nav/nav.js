c34ba07c784fbc7db8add6b462100c05b08bfabe
Kyle Patrick Bader <kbader91@gmail.com>
2022-12-09 09:21:46 -0800

    updated resume

--- src/components/Nav/nav.js
+++ src/components/Nav/nav.js
@@ -23,7 +23,7 @@ function Nav() {
                 <Link to="/" style={activeTab("/kpb-portfolio-by-react")} className="title">Home</Link>
                 <Link to="/About" style={activeTab("/About")} className="title">About</Link>
                 <Link to="/Projects" style={activeTab("/Projects")} className="title">Projects</Link>
-                <a href="https://drive.google.com/file/d/1vYb40_bSa_p_XdIWE8XY6Y_B1RfsDlgn/view?usp=sharing" target="_blank" rel="noreferrer">
+                <a href="https://drive.google.com/file/d/1g1Hi0i8K4IizfJearQ79lUEJyH59QUSP/view?usp=sharing" target="_blank" rel="noreferrer">
                     Resume
                 </a>
                 <Link to="/Contact" style={activeTab("/Contact")} className="title">Contact</Link>
